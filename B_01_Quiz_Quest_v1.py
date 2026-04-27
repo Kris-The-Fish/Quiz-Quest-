e14063a9c56784c713fd6771b35aec37fae17ef6
Kris-The-Fish <witehiram4172@masseyhigh.school.nz>
2026-04-28 09:46:18 +1200

    Add files via upload

--- B_01_Quiz_Quest_v1.py
+++ B_01_Quiz_Quest_v1.py
@@ -1,7 +1,6 @@
 
 import random
 
-
 def yes_no(question):
 
     """Checks user response to a questin is yes / no (y/), returns 'yes' or 'no' """
@@ -33,10 +32,28 @@ def difficulty(question):
             return "medium"
         elif response == "hard" or response == "h":
             return "hard"
-        elif response == "costume" or response == "c":
-            return "costume"
         else:
-            print("please enter easy / medium / hard / costume")
+            print("please enter easy / medium / hard")
+
+def question_pool(question):
+
+    """Checks user response to a questin is yes / no (y/), returns 'yes' or 'no' """
+
+    while True:
+
+        response = input(question).lower()
+
+        # check the user says yes / no / Y / n
+        if response == "+":
+           return "+"
+        elif response == "-":
+            return "-"
+        elif response == "x":
+            return "x"
+        elif response == "/":
+            return "/"
+        else:
+            print("please enter + \ - \ x \ /")
 
 def int_check(question, low=None, high=None, exit_code=None):
 
@@ -93,7 +110,8 @@ def instructions():
   You'll be ask questions a series of multiple different math questions.
 ------------------------------------------------------------------------
 - Choose how many rounds you would like, or press <enter> for infinity. 
-- Choose the difficulty or just use the default difficulty (medium).   
+- Pick the difficulty you would like.
+- Pick the operation you would like.   
 - Try to answer the questions the best you can!
 - You can exit the game by entering <xxx>.
 - at the end you'll be able to see your history, if you want.
@@ -115,14 +133,12 @@ def generate_subtraction(low, high):
 
 def generate_multiplication(low, high):
     for _ in range(1000):
-        i = random.randint(low, high)
+
         answer = random.randint(low, high)
 
-        factors = [(i, answer * i) for i in range(low, high + 1)
-                   if answer * i == 0 and low <= answer * i <= high]
+        factors = [(i, answer // i) for i in range(low, high + 1)
+                   if answer % i == 0 and low <= answer / i <= high]
 
-        if answer or i == 0:
-            continue
 
         if factors:
             num1, num2 = random.choice(factors)
@@ -142,12 +158,9 @@ def generate_division(low, high):
     # fallback
     return generate_addition(low, high)
 
-def repeat_questions():
-    print()
-
 # Main routine
 print()
-print(" === Quiz Quest === ")
+print(" === Number Quest === ")
 print()
 
 # ask the user if they want instructions (check they say yes / no)
@@ -156,6 +169,9 @@ want_instructions = yes_no("Do you want to see the instructions? ")
 # Display the instructions if the user wants to see them...
 if want_instructions == "yes":
     instructions()
+if want_instructions == "no":
+    print("You've picked to NOT see the instructions ")
+print()
 
 mode = "regular"
 rounds_played = 0
@@ -164,54 +180,51 @@ feedback = ""
 rounds_lost = 0
 guesses_allowed = 1
 question_history = set()
+operation = ""
 
 game_history = []
 all_scores = []
 
 # ask user for number of rounds / infinite mode
-num_rounds = int_check("Rounds <enter for infinite>: ",
-                       low=1, exit_code="")
+num_rounds = int_check("How many rounds ? <enter for infinite>: ", low=1, exit_code="")
 
 if num_rounds == "":
     mode = "infinite"
     num_rounds = 1
+    print("You are now in infinite mode !")
+print()
 
-# ask user if they want to customise the number range
-default_params = yes_no("Do you want to use the default game difficulty? ")
-if default_params == "yes":
-    low_num = 0
-    high_num = 100
+# let user pick the operation (+, -, X, /)
+pool_questions = question_pool("Pick which operation you would like (+, -, X, /) ")
+if pool_questions == "+":
+    operation = "+"
+    print("You've picked addition!")
+elif pool_questions == "-":
+    operation = "-"
+    print("You've picked subtraction!")
+elif pool_questions == "x":
+    operation = "x"
+    print("You've picked multiplication")
+elif pool_questions == "/":
+    operation = "/"
+    print("You've picked division")
+print()
 
-# allow user to choose difficulty
-else:
-
-    difficult = difficulty(
-        "what difficulty would you like? (Easy, Medium, Hard) or type <costume> for costume parameter ")
-
-    if difficult == "easy":
+# let user pick the difficulty (Easy, Medium, Hard)
+difficult = difficulty("what difficulty would you like? (Easy, Medium, Hard) ")
+if difficult == "easy":
         low_num = 1
-        high_num = 12
-
-    elif difficult == "medium":
+        high_num = 10
+        print("You've picked easy")
+elif difficult == "medium":
         low_num = 1
         high_num = 101
-
-    elif difficult == "hard":
-        low_num = 1
+        print("You've picked medium")
+elif difficult == "hard":
+        low_num = 10
         high_num = 2763
-
-    else:
-
-        print("""
-                    ⚠️⚠️⚠️ WARNING ⚠️⚠️⚠️
-
-            <0> does not work ! do NOT enter <0> ! 
-            However negative numbers do work. 
-            (also make sure you're using full numbers) 
-
-        """)
-        low_num = int_check("Low Number? ")
-        high_num = int_check("High Number? ", low=low_num + 1)
+        print("You've picked hard")
+print()
 
 # Game loop starts
 while rounds_played < num_rounds:
@@ -222,22 +235,25 @@ while rounds_played < num_rounds:
     guesses_used = 0
     already_guessed = []
 
-    # pick question at random
+    # pick question correctly
     while True:
-        operation = random.choice(["+", "-", "x", "/"])
+
         if operation == "+":
             num1, num2, answer, op = generate_addition(low_num, high_num)
         elif operation == "-":
             num1, num2, answer, op = generate_subtraction(low_num, high_num)
         elif operation == "x":
             num1, num2, answer, op = generate_multiplication(low_num, high_num)
-        else:
+        elif operation == "/":
             num1, num2, answer, op = generate_division(low_num, high_num)
 
         question = f"{num1} {op} {num2}"
         if question not in question_history:
             question_history.add(question)
             break
+        else:
+            break
+
 
     user_answer = int_check(f"{question} = ", exit_code= "xxx")
 
@@ -250,19 +266,20 @@ while rounds_played < num_rounds:
     # if user answered right or wrong, print the right response
     if user_answer == answer:
         feedback = "✅✅✅ correct! Good Job! You got it! ✅✅✅"
-
     else:
         feedback = f"❌❌❌ WRONG! The answer is [{answer}], you silly! You'll get it eventually (maybe). ❌❌❌"
         rounds_lost += 1
-
     print(feedback)
 
+    # infinite
     if mode == "infinite":
         num_rounds += 1
 
+    # end game
     if end_game == "yes":
         break
 
+    # calculate the score
     rounds_won = rounds_played - rounds_lost
     percent_won = rounds_won / rounds_played * 100
     percent_lost = rounds_lost / rounds_played * 100
@@ -270,46 +287,46 @@ while rounds_played < num_rounds:
     # history
     history_feedback = (
         f"Question {rounds_played}: {num1} {op} {num2} = {answer} | {feedback} "
-        f" won: {percent_won:.2f}% | Lost: {percent_lost:.2f}%"
+        f" You got: {percent_won:.2f}% Corrct | {percent_lost:.2f}% wrong "
     )
     game_history.append(history_feedback)
+print()
 
 # print history if the user wants it
 if rounds_played > 0:
-    # calculate statistics
+    # calculate statistics (again)
     rounds_won = rounds_played - rounds_lost
     percent_won = rounds_won / rounds_played * 100
     percent_lost = rounds_lost / rounds_played * 100
 
-
-
     # output game statistics
     print()
     print("📊📊📊Game Statistics📊📊📊")
-    print(f"👍Won: {percent_won: .2f}% \t "
-          f"😢Lost: {percent_lost:.2f}% \t ")
+    print(f"👍Correct: {percent_won: .2f}% \t "
+          f"😢wrong: {percent_lost:.2f}% \t ")
 
     # Ask user if they want to see their game history output if it requested
     see_history = string_checker("\nDo you want to see your Game History? ")
+    print()
     if see_history == "yes":
         for item in game_history:
-            print()
             print(item)
 
-        print("""
+        print(f"""
         Just incase you forgot, here are the
         📊📊📊Game Statistics📊📊📊 again
         
         👍Won: {percent_won: .2f}% \t
         😢Lost: {percent_lost:.2f}% \t
         """)
+print()
 
+# if the user haven't played any rounds, say they have no history
 if rounds_played == 0:
     percent_won = percent_lost = 0
     print()
     print("NO history available! Try actually playing the game. ")
-
-
+    print()
 print()
-print("!!! Thanks for playing !!!")
 
+print("!!! Thanks for playing !!!")
